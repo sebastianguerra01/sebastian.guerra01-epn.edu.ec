12416b57a988cb96e912ede88a2496da0dd129ec
Sebas <sebastiantguerra.2003@gmail.com>
2023-03-12 11:49:40 -0500

    Cambio 3

---
+++ src/BusinessLogic/Entities/sgUsers.java
@@ -0,0 +1,12 @@
+package BusinessLogic.Entities;
+
+public class sgUsers {
+    String sgUsuarios;
+    String sgContrasena;
+
+    public sgUsers(String sgUsuarios, String sgContrasena) {
+        this.sgUsuarios = sgUsuarios;
+        this.sgContrasena = sgContrasena;
+    }
+}
+
